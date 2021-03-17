55bc1b0682a927014184caffd9648b376151b35a
AmitKumar <amit.kumar.ism@gmail.com>
2021-03-17 14:09:34 -0700

    added the application file

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment/index";
 
 const days = [
   {
@@ -21,6 +22,24 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
 
 
 
@@ -53,7 +72,14 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      {
+          appointments.map(appointment => {
+            return (
+              <Appointment key={appointment.id} {...appointment} />
+            );
+          })
+        }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
