9f3609d7d76d52ae952ec5d9efdca1afe783a50a
AmitKumar <amit.kumar.ism@gmail.com>
2021-03-18 21:28:44 -0700

    added the getInterviewersForDay function and tested with some condition

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,3 +18,14 @@ export function getInterview(state, interview) {
   }
   return null;
 }
+
+
+export  function getInterviewersForDay(state, day) {
+  const [filteredDay] = state.days.filter(days => days.name === day);
+
+  if (!filteredDay) 
+  return [];
+
+  const interviewersArray = filteredDay.interviewers.map(interview => state.interviewers[interview]);
+  return interviewersArray;
+};
