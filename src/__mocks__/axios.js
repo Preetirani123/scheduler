fc3acf363fddd5b62e35595848c22df0348b759a
AmitKumar <amit.kumar.ism@gmail.com>
2021-03-23 19:51:37 -0700

    added the cancel an interviwer test

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -89,6 +89,13 @@ export default {
       statusText: "No Content",
     });
   }),
+  delete: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
+
+})
     
 };
 
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,7 +8,11 @@ import { render,
    prettyDOM,
    getAllByTestId,
    getByPlaceholderText,
-   getByAltText
+   getByAltText,
+   queryByText,
+   queryByAltText,
+   
+   
    
    
 } from "@testing-library/react";
@@ -30,22 +34,58 @@ it("changes the schedule when a new day is selected", async () => {
 });
 
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-  const { container } = render(<Application />);
+  const { container, debug } = render(<Application />);
 
   await waitForElement(() => getByText(container, "Archie Cohen"));
+
   const appointments = getAllByTestId(container, "appointment");
-  const appointment = getAllByTestId(container, "appointment")[0];
-  
+  const appointment = appointments[0];
+
   fireEvent.click(getByAltText(appointment, "Add"));
 
-fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-  target: { value: "Lydia Miller-Jones" }
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" }
+  });
+
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  fireEvent.click(getByText(appointment, "Save"));
+
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 });
-fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
-fireEvent.click(getByText(appointment, "Save"));
-console.log(prettyDOM(appointment));
+it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  // 1. Render the Application.
+  const { container, debug } = render(<Application />);
 
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
 
+  // 3. Click the "Delete" button on the booked appointment.
+  const appointment = getAllByTestId(container, "appointment").find(
+    appointment => queryByText(appointment, "Archie Cohen")
+  );
+
+  fireEvent.click(queryByAltText(appointment, "Delete"));
+  expect(getByText(appointment, 'Delete the appointment?')).toBeInTheDocument();
+  
+  fireEvent.click(getByText(appointment, "Confirm"));
+
+  expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+  await waitForElement(() => getByAltText(appointment, "Add"));
+
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
 });
 });
