34d0b22a2fcd7e7b6afc73b8a92a6784ddbe0643
AmitKumar <amit.kumar.ism@gmail.com>
2021-03-24 11:25:44 -0700

    fix the unique key error warning in DayList conponent

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,18 +4,14 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {
   const day = props.days.map(day => {
     return (
-        <ul>
           <DayListItem 
           key={day.id}
           name={day.name} 
           spots={day.spots} 
           selected={day.name === props.day}
-          setDay={props.setDay}  />
-
-        </ul>
+          setDay={props.setDay}  />    
     );
   });
-
-  return day;
+return <ul>{day}</ul>;
 }
 
